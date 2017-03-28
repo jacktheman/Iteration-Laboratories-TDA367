da70f981021b61170a27fa5c0bcb997aa4066d22
Aron Sandstedt <Aron.Sandstedt@live.se>
2017-03-29 00:42:21 +0200

    Created the Main Page of the program using fxml Created a css file for the fxml file. Created a controller for the fxml file.

---
+++ src/main/java/MainPageController.java
@@ -0,0 +1,13 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by aron on 2017-03-29.
+ */
+public class MainPageController implements Initializable{
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
