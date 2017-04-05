484fd398be50f53de57bc7d2ece0f0212c0e2060
Cutecurtain <Aron.Sandstedt@live.se>
2017-04-05 12:33:48 +0200

    Created a test implementation of TextContainer.

--- src/main/java/NoteObjects/Objects/TextContainer.java
+++ src/main/java/NoteObjects/Objects/TextContainer.java
@@ -1,10 +1,63 @@
 package NoteObjects.Objects;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
 /**
  * Created by aron on 2017-04-03.
  */
 public class TextContainer extends NoteObject {
-    void initPane() {
 
+    /**
+     * The actual area in which the user writes.
+     */
+    private TextArea textArea;
+
+    /**
+     * A Text object for resizing the TextArea.
+     *
+     * Purely for logic. The TextArea contains a ScrollPane,
+     * which makes it so that we can't resize it dynamically
+     * without extra information about the contents size on screen.
+     */
+    private Text textHolder;
+
+    public TextContainer(int xPos, int yPos) {
+        super(xPos, yPos);
+        textArea = new TextArea();
+        textHolder = new Text();
+        textArea.setPrefWidth(50);
+        textArea.setPrefHeight(50);
+
+        textArea.setWrapText(true);
+        textHolder.textProperty().bind(textArea.textProperty());
+
+        initPane();
+    }
+
+    private void listener() {
+        textArea.textProperty().addListener(e -> {
+            int newWidth = (int) textHolder.getLayoutBounds().getWidth() + 30; // + 30 because padding
+            int newHeight = (int) textHolder.getLayoutBounds().getHeight() + 30;
+            if (newWidth != textArea.getPrefWidth())
+                textArea.setPrefWidth(newWidth);
+            if (newHeight != textArea.getPrefHeight())
+                textArea.setPrefHeight(newHeight);
+        });
+    }
+
+    private void listener2() {
+        textArea.focusedProperty().addListener(e -> {
+            if (!textArea.isFocused() && textArea.getText().equals("")) {
+                removeFromPane(textArea);
+            }
+        });
+    }
+
+    @Override
+    void initPane() {
+        listener();
+        listener2();
+        addToPane(textArea);
     }
 }
--- src/main/java/PageControllers/MainPageController.java
+++ src/main/java/PageControllers/MainPageController.java
@@ -1,5 +1,9 @@
 package PageControllers;
 
+import NoteObjects.Objects.TextContainer;
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -7,6 +11,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.Control;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -27,6 +33,8 @@ public class MainPageController implements Initializable{
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         instance = this;
+        notePane.setStyle("-fx-background-color: white");
+        pressOnCanvas();
     }
 
     public void addToNotePane(Node... nodes) {
@@ -35,5 +43,17 @@ public class MainPageController implements Initializable{
 
     public void removeToNotePane(Node... nodes) {
         notePane.getChildren().removeAll(nodes);
+    }
+
+    private void pressOnCanvas() {
+        notePane.setOnMousePressed(mouseEvent -> {
+            if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                TextContainer textContainer = new TextContainer((int) mouseEvent.getX(), (int) mouseEvent.getY());
+                Pane pane = textContainer.getPane();
+                addToNotePane(pane);
+                //pane.requestFocus();
+                //pane.getChildren().get(pane.getChildren().size()-1).requestFocus();
+            }
+        });
     }
 }
