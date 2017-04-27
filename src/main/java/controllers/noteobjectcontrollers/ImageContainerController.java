df39a941ca96e2c89a83ed4496844b84a58559cc
Skroudelz <s.bennhage@gmail.com>
2017-04-27 15:15:39 +0200

    Moving images implemented

--- src/main/java/controllers/noteobjectcontrollers/ImageContainerController.java
+++ src/main/java/controllers/noteobjectcontrollers/ImageContainerController.java
@@ -18,24 +18,15 @@ public class ImageContainerController extends NoteObjectController<ImageContaine
     public ImageContainerController(Image image, double layoutX, double layoutY){
         super(new ImageContainerView(image, layoutX, layoutY));
         this.imageContainerModel = new ImageContainerModel(image);
-        //listener();
     }
 
     public ImageContainerController(URL url, double layoutX, double layoutY){
         super(new ImageContainerView(url,layoutX, layoutY));
         this.imageContainerModel = new ImageContainerModel(new Image(url.toString()));
-        //listener();
     }
 
     public Image returnModelImage(){
         return imageContainerModel.getImage();
     }
-
-    /*public void listener(){
-        super.getNode().setOnMousePressed((MouseEvent event) -> {
-            super.getNode().requestFocus();
-            System.out.println("Clicked on ImageContainer");
-        });
-    }*/
 
 }
--- src/main/java/controllers/noteobjectcontrollers/NoteObjectController.java
+++ src/main/java/controllers/noteobjectcontrollers/NoteObjectController.java
@@ -1,9 +1,17 @@
 package controllers.noteobjectcontrollers;
 
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.effect.Light;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 /**
  * Created by svante on 2017-04-06.
@@ -14,18 +22,39 @@ abstract class NoteObjectController<T extends Node> implements NoteObjectControl
 
     private List<NoteObjectObserverI> listeners;
 
+    private double dragx, dragy;
+
     NoteObjectController(T view) {
         this.view = view;
         this.listeners = new ArrayList<>();
+        drag();
     }
 
     private void drag() {
-
+        view.setOnMousePressed(mouseEvent -> {
+            view.requestFocus();
+            dragx = mouseEvent.getX();
+            dragy = mouseEvent.getY();
+            System.out.println("ddd");
+            view.getScene().setCursor(Cursor.MOVE);
+        });
+        view.setOnMouseReleased(mouseEvent -> view.getScene().setCursor(Cursor.HAND));
+        view.setOnMouseDragged(mouseEvent -> {
+            view.setLayoutX(view.getLayoutX() + mouseEvent.getX() - dragx);
+            view.setLayoutY(view.getLayoutY() + mouseEvent.getY() - dragy);
+        });
+        view.setOnMouseEntered(mouseEvent -> {
+            if (!mouseEvent.isPrimaryButtonDown()) {
+                view.getScene().setCursor(Cursor.HAND);
+            }
+        });
+        view.setOnMouseExited(mouseEvent -> {
+            if (!mouseEvent.isPrimaryButtonDown()) {
+                view.getScene().setCursor(Cursor.DEFAULT);
+            }
+        });
     }
 
-    private void drop() {
-
-    }
 
     private void onRightClick() {
 
