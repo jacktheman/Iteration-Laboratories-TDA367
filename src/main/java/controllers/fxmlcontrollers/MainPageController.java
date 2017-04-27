0d56451d89af9df4c73772b0ca42b320ee487fa9
Skroudelz <s.bennhage@gmail.com>
2017-04-27 14:28:14 +0200

    Selection of images implemented

--- src/main/java/controllers/fxmlcontrollers/MainPageController.java
+++ src/main/java/controllers/fxmlcontrollers/MainPageController.java
@@ -1,15 +1,19 @@
 package controllers.fxmlcontrollers;
 
 import controllers.noteobjectcontrollers.ImageContainerController;
+import controllers.noteobjectcontrollers.NoteObjectControllerI;
 import controllers.noteobjectcontrollers.TextContainerController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import models.notemodel.NoteModel;
 import services.FileChooserFactory;
+import views.noteobjectviews.ImageContainerView;
+import views.noteobjectviews.TextContainerView;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,7 +26,7 @@ import java.util.ResourceBundle;
  */
 public class MainPageController implements Initializable {
 
-   @FXML
+    @FXML
     private AnchorPane notePane;
 
     private NoteModel currentNote;
@@ -33,11 +37,28 @@ public class MainPageController implements Initializable {
 
     private void pressedOnCanvas() {
         notePane.setOnMouseReleased(e -> {
-            if (e.getButton().equals(MouseButton.PRIMARY))
-                addTextToNote(e.getX(), e.getY());
-            else if (e.getButton().equals(MouseButton.SECONDARY))
-                addImageToNote(e.getX(), e.getY());
+            Node focusOwner = notePane.getScene().getFocusOwner();
+            boolean addToNotePane = true;
+            if (notePane.getChildren().contains(focusOwner)) {
+                double x1 = focusOwner.getLayoutX();
+                double x2 = focusOwner.getLayoutX() + focusOwner.getBoundsInLocal().getWidth();
+                double y1 = focusOwner.getLayoutY();
+                double y2 = focusOwner.getLayoutY() + focusOwner.getBoundsInLocal().getHeight();
+
+                if ((e.getX() >= x1 && e.getX() <= x2 && e.getY() >= y1 && e.getY() <= y2)) {
+                    addToNotePane = false;
+                }
+
+            }
+
+            if (addToNotePane) {
+                if (e.getButton().equals(MouseButton.PRIMARY))
+                    addTextToNote(e.getX(), e.getY());
+                else if (e.getButton().equals(MouseButton.SECONDARY))
+                    addImageToNote(e.getX(), e.getY());
+            }
         });
+
     }
 
     private void addImageToNote(double x, double y) {
@@ -59,11 +80,14 @@ public class MainPageController implements Initializable {
         if (currentNote == null) {
             currentNote = new NoteModel();
         }
-        notePane.requestFocus();
-        currentNote.addNoteObjectController(new TextContainerController("", x, y));
-        notePane.getChildren().clear();
-        notePane.getChildren().addAll(currentNote.getNodes());
-        currentNote.getNodes().get(currentNote.getNodes().size() - 1).requestFocus();
+
+        Node currentFocus = notePane.getScene().getFocusOwner();
+            notePane.requestFocus();
+            currentNote.addNoteObjectController(new TextContainerController("", x, y));
+            notePane.getChildren().clear();
+            notePane.getChildren().addAll(currentNote.getNodes());
+            currentNote.getNodes().get(currentNote.getNodes().size() - 1).requestFocus();
+
     }
 
     @FXML
--- src/main/java/controllers/noteobjectcontrollers/ImageContainerController.java
+++ src/main/java/controllers/noteobjectcontrollers/ImageContainerController.java
@@ -2,6 +2,7 @@ package controllers.noteobjectcontrollers;
 
 import javafx.scene.Node;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import models.noteobjectmodels.ImageContainerModel;
 import views.noteobjectviews.ImageContainerView;
 
@@ -17,15 +18,24 @@ public class ImageContainerController extends NoteObjectController<ImageContaine
     public ImageContainerController(Image image, double layoutX, double layoutY){
         super(new ImageContainerView(image, layoutX, layoutY));
         this.imageContainerModel = new ImageContainerModel(image);
+        //listener();
     }
 
     public ImageContainerController(URL url, double layoutX, double layoutY){
         super(new ImageContainerView(url,layoutX, layoutY));
         this.imageContainerModel = new ImageContainerModel(new Image(url.toString()));
+        //listener();
     }
 
     public Image returnModelImage(){
         return imageContainerModel.getImage();
     }
+
+    /*public void listener(){
+        super.getNode().setOnMousePressed((MouseEvent event) -> {
+            super.getNode().requestFocus();
+            System.out.println("Clicked on ImageContainer");
+        });
+    }*/
 
 }
