5176daba735f87e57149228bf2c8ed9d085b2358
Skroudelz <s.bennhage@gmail.com>
2017-05-03 16:25:35 +0200

    Things to trassla ur

--- src/main/java/controllers/noteobjectcontrollers/ImageContainerController.java
+++ src/main/java/controllers/noteobjectcontrollers/ImageContainerController.java
@@ -7,6 +7,7 @@ import javafx.scene.input.MouseEvent;
 import models.noteobjectmodels.ImageContainerModel;
 import utilities.noteobjectbehaviors.DragDropBehavior;
 import utilities.noteobjectbehaviors.NoteObjectBehaviorI;
+import utilities.noteobjectbehaviors.ResizeBehavior;
 import views.noteobjectviews.ImageContainerView;
 
 import java.net.URL;
@@ -17,11 +18,10 @@ import java.net.URL;
  */
 public class ImageContainerController extends NoteObjectController<ImageContainerView> {
 
-    private enum ResizablePositions {
-        LEFT_UPPER_CORNER, LEFT_LOWER_CORNER, RIGHT_UPPER_CORNER, RIGHT_LOWER_CORNER, UPPER_AREA, LEFT_AREA, BOTTOM_AREA, RIGHT_AREA
-    }
 
     private NoteObjectBehaviorI noteObjectBehavior;
+
+    private ResizeBehavior resizeBehavior;
 
     private ImageContainerModel imageContainerModel;
 
@@ -29,86 +29,18 @@ public class ImageContainerController extends NoteObjectController<ImageContaine
         super(new ImageContainerView(image, layoutX, layoutY));
         this.imageContainerModel = new ImageContainerModel(image);
         noteObjectBehavior = new DragDropBehavior(super.getNode());
-        setOnMouseMoved();
     }
 
     public ImageContainerController(URL url, double layoutX, double layoutY) {
         super(new ImageContainerView(url, layoutX, layoutY));
         this.imageContainerModel = new ImageContainerModel(new Image(url.toString()));
         noteObjectBehavior = new DragDropBehavior(super.getNode());
-        setOnMouseMoved();
     }
 
     public Image returnModelImage() {
         return imageContainerModel.getImage();
     }
 
-    private void setOnMouseMoved() {
-        super.getNode().setOnMouseMoved(mouseEvent -> {
-            ResizablePositions position = returnCursorLocation(mouseEvent);
-            changeCursorBasedOnPosition(position);
-        });
-    }
-
-    private ResizablePositions returnCursorLocation(MouseEvent mouseEvent) {
-        ResizablePositions pos = null;
-
-        if (mouseEvent.getX() >= 0 && mouseEvent.getX() <= 5 && mouseEvent.getY() >= 0 && mouseEvent.getY() <= 5) {
-            pos = ResizablePositions.LEFT_UPPER_CORNER;
-        } else if (mouseEvent.getX() >= 0 && mouseEvent.getX() <= 5 && mouseEvent.getY() >= super.getNode().getFitHeight() - 5 && mouseEvent.getY() <= super.getNode().getFitHeight()) {
-            pos = ResizablePositions.LEFT_LOWER_CORNER;
-        } else if (mouseEvent.getX() >= super.getNode().getFitWidth() - 5 && mouseEvent.getX() <= super.getNode().getFitWidth() && mouseEvent.getY() >= 0 && mouseEvent.getY() <= 5) {
-            pos = ResizablePositions.RIGHT_UPPER_CORNER;
-        } else if (mouseEvent.getX() >= super.getNode().getFitWidth() - 5 && mouseEvent.getX() <= super.getNode().getFitWidth() && mouseEvent.getY() >= super.getNode().getFitHeight() - 5 && mouseEvent.getY() <= super.getNode().getFitHeight()) {
-            pos = ResizablePositions.RIGHT_LOWER_CORNER;
-        } else if (mouseEvent.getY() >= 0 && mouseEvent.getY() <= 5) {
-            pos = ResizablePositions.UPPER_AREA;
-        } else if (mouseEvent.getX() >= 0 && mouseEvent.getX() <= 5) {
-            pos = ResizablePositions.LEFT_AREA;
-        } else if (mouseEvent.getY() >= super.getNode().getFitHeight() - 5 && mouseEvent.getY() <= super.getNode().getFitHeight()) {
-            pos = ResizablePositions.BOTTOM_AREA;
-        } else if (mouseEvent.getX() >= super.getNode().getFitWidth() - 5 && mouseEvent.getX() <= super.getNode().getFitWidth()) {
-            pos = ResizablePositions.RIGHT_AREA;
-        }
-
-        return pos;
-
-    }
-
-    private void changeCursorBasedOnPosition(ResizablePositions pos) {
-
-        if (pos != null) {
-            System.out.println(pos.toString());
-            switch (pos) {
-                case RIGHT_UPPER_CORNER:
-                    super.getNode().setCursor(Cursor.NE_RESIZE);
-                    break;
-                case RIGHT_LOWER_CORNER:
-                    super.getNode().setCursor(Cursor.SE_RESIZE);
-                    break;
-                case LEFT_UPPER_CORNER:
-                    super.getNode().setCursor(Cursor.NW_RESIZE);
-                    break;
-                case LEFT_LOWER_CORNER:
-                    super.getNode().setCursor(Cursor.SW_RESIZE);
-                    break;
-                case BOTTOM_AREA:
-                    super.getNode().setCursor(Cursor.S_RESIZE);
-                    break;
-                case UPPER_AREA:
-                    super.getNode().setCursor(Cursor.N_RESIZE);
-                    break;
-                case RIGHT_AREA:
-                    super.getNode().setCursor(Cursor.E_RESIZE);
-                    break;
-                case LEFT_AREA:
-                    super.getNode().setCursor(Cursor.W_RESIZE);
-                    break;
-            }
-        } else {
-            super.getNode().setCursor(Cursor.HAND);
-        }
-    }
 
 }
 
---
+++ src/main/java/utilities/noteobjectbehaviors/ResizeBehavior.java
@@ -0,0 +1,127 @@
+package utilities.noteobjectbehaviors;
+
+import javafx.scene.Cursor;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * Created by svante on 2017-05-03.
+ */
+public class ResizeBehavior implements NoteObjectBehaviorI {
+    
+    private ImageView imageView;
+
+    private enum ResizablePositions {
+        LEFT_UPPER_CORNER, LEFT_LOWER_CORNER, RIGHT_UPPER_CORNER, RIGHT_LOWER_CORNER, UPPER_AREA, LEFT_AREA, BOTTOM_AREA, RIGHT_AREA
+    }
+
+    public ResizeBehavior(ImageView view){
+        this.imageView = view;
+        setOnMouseMoved();
+        setOnMouseDragged();
+    }
+
+    private void setOnMouseMoved() {
+        imageView.setOnMouseMoved(mouseEvent -> {
+            ResizablePositions position = returnCursorLocation(mouseEvent);
+            changeCursorBasedOnPosition(position);
+        });
+    }
+
+    private void setOnMouseDragged(){
+        imageView.setOnMouseDragged(mouseEvent -> {
+            ResizablePositions resizablePositions = returnCursorLocation(mouseEvent);
+            System.out.println("DragDetected");
+            dragResize(resizablePositions, mouseEvent);
+        });
+    }
+
+    private ResizablePositions returnCursorLocation(MouseEvent mouseEvent) {
+        ResizablePositions pos = null;
+
+        if (mouseEvent.getX() >= 0 && mouseEvent.getX() <= 5 && mouseEvent.getY() >= 0 && mouseEvent.getY() <= 5) {
+            pos = ResizablePositions.LEFT_UPPER_CORNER;
+        } else if (mouseEvent.getX() >= 0 && mouseEvent.getX() <= 5 && mouseEvent.getY() >= imageView.getFitHeight() - 5 && mouseEvent.getY() <= imageView.getFitHeight()) {
+            pos = ResizablePositions.LEFT_LOWER_CORNER;
+        } else if (mouseEvent.getX() >= imageView.getFitWidth() - 5 && mouseEvent.getX() <= imageView.getFitWidth() && mouseEvent.getY() >= 0 && mouseEvent.getY() <= 5) {
+            pos = ResizablePositions.RIGHT_UPPER_CORNER;
+        } else if (mouseEvent.getX() >= imageView.getFitWidth() - 5 && mouseEvent.getX() <= imageView.getFitWidth() && mouseEvent.getY() >= imageView.getFitHeight() - 5 && mouseEvent.getY() <= imageView.getFitHeight()) {
+            pos = ResizablePositions.RIGHT_LOWER_CORNER;
+        } else if (mouseEvent.getY() >= 0 && mouseEvent.getY() <= 5) {
+            pos = ResizablePositions.UPPER_AREA;
+        } else if (mouseEvent.getX() >= 0 && mouseEvent.getX() <= 5) {
+            pos = ResizablePositions.LEFT_AREA;
+        } else if (mouseEvent.getY() >= imageView.getFitHeight() - 5 && mouseEvent.getY() <= imageView.getFitHeight()) {
+            pos = ResizablePositions.BOTTOM_AREA;
+        } else if (mouseEvent.getX() >= imageView.getFitWidth() - 5 && mouseEvent.getX() <= imageView.getFitWidth()) {
+            pos = ResizablePositions.RIGHT_AREA;
+        }
+
+        return pos;
+
+    }
+
+    private void dragResize(ResizablePositions position, MouseEvent event){
+
+        double nodeX, nodeY, mouseX, mouseY;
+        mouseX = event.getX();
+        mouseY = event.getY();
+        nodeX = imageView.getFitWidth();
+        nodeY = imageView.getFitHeight();
+
+        if(position == ResizablePositions.LEFT_UPPER_CORNER){
+
+        } else if (position == ResizablePositions.LEFT_LOWER_CORNER){
+
+        } else if (position == ResizablePositions.RIGHT_UPPER_CORNER){
+
+        } else if (position == ResizablePositions.RIGHT_LOWER_CORNER){
+
+        } else if (position == ResizablePositions.UPPER_AREA){
+
+        } else if (position == ResizablePositions.LEFT_AREA){
+
+        } else if (position == ResizablePositions.BOTTOM_AREA){
+
+        } else if (position == ResizablePositions.RIGHT_AREA){
+            imageView.setFitWidth(nodeX + mouseX - nodeX);
+        }
+    }
+
+    private void changeCursorBasedOnPosition(ResizablePositions pos) {
+
+        if (pos != null) {
+            //System.out.println(pos.toString());
+            switch (pos) {
+                case RIGHT_UPPER_CORNER:
+                    imageView.setCursor(Cursor.NE_RESIZE);
+                    break;
+                case RIGHT_LOWER_CORNER:
+                    imageView.setCursor(Cursor.SE_RESIZE);
+                    break;
+                case LEFT_UPPER_CORNER:
+                    imageView.setCursor(Cursor.NW_RESIZE);
+                    break;
+                case LEFT_LOWER_CORNER:
+                    imageView.setCursor(Cursor.SW_RESIZE);
+                    break;
+                case BOTTOM_AREA:
+                    imageView.setCursor(Cursor.S_RESIZE);
+                    break;
+                case UPPER_AREA:
+                    imageView.setCursor(Cursor.N_RESIZE);
+                    break;
+                case RIGHT_AREA:
+                    imageView.setCursor(Cursor.E_RESIZE);
+                    break;
+                case LEFT_AREA:
+                    imageView.setCursor(Cursor.W_RESIZE);
+                    break;
+            }
+        } else {
+            imageView.setCursor(Cursor.HAND);
+        }
+    }
+
+
+}
