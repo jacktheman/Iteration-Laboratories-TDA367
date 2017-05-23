3ed62e55881d3daef389c56a1015c84aa66db3a1
Cutecurtain <aron.sandstedt@live.se>
2017-05-23 23:34:16 +0200

    Removed unnecessary objects in XMLReaderTest.

--- src/test/java/com/itlabs/fabnotes/service/filemanagment/XMLReaderTest.java
+++ src/test/java/com/itlabs/fabnotes/service/filemanagment/XMLReaderTest.java
@@ -1,14 +1,10 @@
 package com.itlabs.fabnotes.service.filemanagment;
 
-import com.itlabs.fabnotes.noteobject.model.NoteObjectI;
 import com.itlabs.fabnotes.noteobject.model.TextContainer;
 import com.itlabs.fabnotes.service.NoteSave;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,18 +13,6 @@ import static org.junit.Assert.*;
  */
 public class XMLReaderTest {
     private static final String TEST_STRING = "Test";
-    private static final int TEST_NUMBER = 0;
-
-    private static List<String> tags;
-    private static List<NoteObjectI> models;
-
-    @BeforeClass
-    public static void initTest() {
-        tags = new ArrayList<>();
-        models = new ArrayList<>();
-        tags.add(TEST_STRING);
-        models.add(new TextContainer(TEST_STRING, TEST_NUMBER, TEST_NUMBER));
-    }
 
     @Test
     public void readXMLToNote() throws Exception {
